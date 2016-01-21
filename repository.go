bc72d8e0d10eada832b291096e97886232f25d7c
nathandao <nathan@guynathan.com>
2016-01-21 22:37:39 +0200

    repo fetch method

--- repository.go
+++ repository.go
@@ -1,11 +1,7 @@
 package git2neo4j
 
 import (
-	"log"
-	"os"
-	"path/filepath"
-
-	"github.com/lemmi/git"
+	git "github.com/libgit2/git2go"
 )
 
 // Repository contains a path string to the repository, relative to the folder
@@ -13,24 +9,69 @@ import (
 type Repository struct {
 	// Path to the .git file of the repository, which is obtained by using
 	// the parameter --mirror when cloning.
-	// Eg: git clone --mirror git@your_repo.git
+	// Eg: git clone --mirror git@github.com:youaccount/repo.git
 	Path string
+	// Credentials contains the required ssh credentials to perform remote
+	Credentials
 }
 
-// Getinfo retrieve all the required information about the repository and return
-// a RepositoryInfo type.
-func (rp *Repository) GetInfo() ([]string, error) {
-	wd, err := os.Getwd()
+// Git2goRepo returns the git2go.Repository struct from the Repository's path.
+func (r *Repository) Git2goRepo() (*git.Repository, error) {
+	repo, err := git.OpenRepository(r.Path)
+	return repo, err
+}
+
+// FetchRemotes executes fetch from remotes. This will update info of all
+// remote refspecs.
+func (v *Repository) FetchRemotes() error {
+	r, err := v.Git2goRepo()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	repository, err := git.OpenRepository(filepath.Join(wd, rp.Path))
+	defer r.Free()
+	// Iterate through all remotes and perform fetches.
+	rnames, err := r.Remotes.List()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	cc, err := repository.GetBranches()
-	if err != nil {
-		return nil, err
+	// Remote callbacks that contains required ssh credentials.
+	remotecb := git.RemoteCallbacks{
+		CredentialsCallback:      v.CredentialsCallback,
+		CertificateCheckCallback: v.CertificateCheckCallback,
 	}
-	return cc, nil
+	// Fetch options.
+	fetchoptions := git.FetchOptions{
+		RemoteCallbacks: remotecb,
+		DownloadTags:    git.DownloadTagsAuto,
+		Prune:           git.FetchPruneOn,
+	}
+	for _, rname := range rnames {
+		remote, err := r.Remotes.Lookup(rname)
+		if err != nil {
+			return err
+		}
+		defer remote.Free()
+		// Get refspecs list to be fetched.
+		refspecs, err := remote.FetchRefspecs()
+		if err != nil {
+			return err
+		}
+		// Perform the fetch.
+		if err = remote.Fetch(refspecs, &fetchoptions, ""); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// CredentialsCallback is provided as one of the RemoteCallbacks functions
+// during fetch.
+func (r *Repository) CredentialsCallback(url string, uname_from_url string, allowed_types git.CredType) (git.ErrorCode, *git.Cred) {
+	return r.Credentials.LibgitCred()
+}
+
+// CertificateCheckCallback is provided as one of the RemoteCallbacks functions
+// during fetch.
+func (r *Repository) CertificateCheckCallback(cert *git.Certificate, valid bool, hostname string) git.ErrorCode {
+	return 0
 }
