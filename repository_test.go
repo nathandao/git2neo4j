9a7aa31d81f879532a6b3d67dcb3c1cd0dfb4e52
nathandao <nathan@guynathan.com>
2016-01-21 22:36:47 +0200

    repo fetch method and initial tests

---
+++ repository_test.go
@@ -0,0 +1,106 @@
+package git2neo4j_test
+
+import (
+	"log"
+	"os"
+	"os/user"
+	"path"
+	"testing"
+
+	git "github.com/libgit2/git2go"
+	"github.com/nathandao/git2neo4j"
+)
+
+// Ensures git2go Repository struct can be created from a Repository struct.
+func TestGit2goRepo(t *testing.T) {
+	generateTestRepo()
+	cred := dummyCredentials()
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	r := git2neo4j.Repository{
+		Path: path.Join(wd, "test_dir/dummy_repo"),
+		Credentials: git2neo4j.Credentials{
+			Username:   cred.Username,
+			Publickey:  cred.Publickey,
+			Privatekey: cred.Privatekey,
+			Passphrase: cred.Passphrase,
+		},
+	}
+	repo, err := r.Git2goRepo()
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Last check. If repo is a true repo, it should be freed!
+	repo.Free()
+}
+
+// Credentials acts as a helper to test git2neo4j Credentials.
+type Credentials struct {
+	Username   string
+	Publickey  string
+	Privatekey string
+	Passphrase string
+}
+
+const (
+	DummyRepoUrl string = "git@github.com:nathandao/git-to-json.git"
+)
+
+// generateTestRepo clones a dummy repository to be used for testing.
+func generateTestRepo() {
+	// Check if dummy_repo exists and only clone if repo does not exist.
+	if _, err := os.Stat("test_dir/dummy_repo"); err != nil {
+		log.Print("Dummy repository not found, cloning to test_dir/dummy_repo...")
+		// Check if dummy_repo is a directory
+		cloneOptions := git.CloneOptions{}
+		cloneOptions.FetchOptions = &git.FetchOptions{
+			RemoteCallbacks: git.RemoteCallbacks{
+				CredentialsCallback:      credentialsCallback,
+				CertificateCheckCallback: certificateCheckCallback,
+			},
+		}
+		_, err := git.Clone("git@github.com:nathandao/git-to-json.git", "test_dir/", &cloneOptions)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Print("Dummy repository found.")
+	}
+}
+
+// credentials generates a reusable Credentials struct.
+func dummyCredentials() *Credentials {
+	u, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	uname := "git"
+	publickey := path.Join(u.HomeDir, ".ssh/id_rsa.pub")
+	privatekey := path.Join(u.HomeDir, ".ssh/id_rsa")
+	passphrase := ""
+	return &Credentials{
+		Username:   uname,
+		Publickey:  publickey,
+		Privatekey: privatekey,
+		Passphrase: passphrase,
+	}
+}
+
+func credentialsCallback(url string, username string, allowedTypes git.CredType) (git.ErrorCode, *git.Cred) {
+	u, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	uname := "git"
+	publickey := path.Join(u.HomeDir, ".ssh/id_rsa.pub")
+	privatekey := path.Join(u.HomeDir, ".ssh/id_rsa")
+	passphrase := ""
+	ret, cred := git.NewCredSshKey(uname, publickey, privatekey, passphrase)
+	return git.ErrorCode(ret), &cred
+}
+
+func certificateCheckCallback(cert *git.Certificate, valid bool, hostname string) git.ErrorCode {
+	return 0
+}
