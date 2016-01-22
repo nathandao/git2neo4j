8e08651a48e18c50dbb1249eaf6dc9c6feb0ee09
nathandao <nathan@guynathan.com>
2016-01-22 23:11:56 +0200

    experimenting getting diff stats inside revwalk

--- repository.go
+++ repository.go
@@ -1,6 +1,8 @@
 package git2neo4j
 
 import (
+	"fmt"
+
 	git "github.com/libgit2/git2go"
 )
 
@@ -64,6 +66,45 @@ func (v *Repository) FetchRemotes() error {
 	return nil
 }
 
+func (v *Repository) Test() error {
+	r, err := v.Git2goRepo()
+	if err != nil {
+		return err
+	}
+	defer r.Free()
+	// Initiate the walk.
+	revWalk, err := r.Walk()
+	if err != nil {
+		return err
+	}
+	defer revWalk.Free()
+	// Iterate through all refs to get the starting Oid for the walk.
+	referenceIterator, err := r.NewReferenceIterator()
+	if err != nil {
+		return err
+	}
+	defer referenceIterator.Free()
+	for {
+		reference, err := referenceIterator.Next()
+		if err != nil {
+			break
+		}
+		defer reference.Free()
+		if isRemote := reference.IsRemote(); isRemote == true {
+			if target := reference.Target(); target != nil {
+				if err = revWalk.Push(target); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	err = revWalk.Iterate(revWalkHandler)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // CredentialsCallback is provided as one of the RemoteCallbacks functions
 // during fetch.
 func (r *Repository) CredentialsCallback(url string, uname_from_url string, allowed_types git.CredType) (git.ErrorCode, *git.Cred) {
@@ -74,4 +115,39 @@ func (r *Repository) CredentialsCallback(url string, uname_from_url string, allo
 // during fetch.
 func (r *Repository) CertificateCheckCallback(cert *git.Certificate, valid bool, hostname string) git.ErrorCode {
 	return 0
+}
+
+func remoteBranchIteratorHandler(branch *git.Branch, branchType git.BranchType) error {
+	return nil
+}
+
+func revWalkHandler(commit *git.Commit) bool {
+	// Get current commit's tree and its first parent's tree.
+	commitTree, err := commit.Tree()
+	if err != nil {
+		panic(err)
+	}
+	if parent := commit.Parent(0); parent != nil {
+		parentTree, err := parent.Tree()
+		if err != nil {
+			panic(err)
+		}
+		// Compare the 2 trees to get diff stats.
+		r := commit.Owner()
+		diff, err := r.DiffTreeToTree(parentTree, commitTree, nil)
+		if err != nil {
+			panic(err)
+		}
+		defer diff.Free()
+		diffStat, err := diff.Stats()
+		if err != nil {
+			panic(err)
+		}
+		defer diffStat.Free()
+		deletions := diffStat.Deletions()
+		insertions := diffStat.Insertions()
+		fmt.Println("Additions:", insertions)
+		fmt.Println("Deletions:", deletions)
+	}
+	return true
 }
